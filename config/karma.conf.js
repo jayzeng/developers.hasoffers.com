916a2e2f6e8ea0c7e9c5343b925766ce1a9c9fda
Jay Zeng <jayzeng@jay-zeng.com>
2013-05-21 19:13:16 +0000

    * Added test set up code

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -53,7 +53,7 @@ autoWatch = true;
 // - Safari (only Mac)
 // - PhantomJS
 // - IE (only Windows)
-browsers = ['Chrome'];
+browsers = ['PhantomJS'];
 
 
 // If browser does not capture in given timeout [ms], kill it
